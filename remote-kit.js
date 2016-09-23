6999adcb3cd1762752f0ebe738aa3b4f5cd0f4bb
Seven Yu <dofyyu@gmail.com>
2016-09-23 17:31:40 +0800

    kit completed!!!

--- remote-kit.js
+++ remote-kit.js
@@ -1,7 +1,22 @@
-var app = require('express')(),
+var express = require('express'),
+    bdp = require('body-parser'),
+    app = express(),
     port = 8999;
 
-app.use('/', require('./lib/routers/main.js'));
+app.use(bdp.urlencoded({
+    extended: false
+}));
+
+app.use(express.static('static', {
+    index: false,
+    maxAge: '7 days'
+}));
+
+app.use('/exec', require('./lib/routers/exec.js'));
+
+app.use('/*', function(req, res, next) {
+    res.status(404).send('NOT FOUND.');
+});
 
 app.listen(port, function(err) {
     console.log('--== Remote Kit ==--');
